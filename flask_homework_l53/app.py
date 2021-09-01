0098b140ab8eda6bf042557eb732544a67a8b6e5
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-01 11:14:28 +0500

    add func add_task and template

--- flask_homework_l53/app.py
+++ flask_homework_l53/app.py
@@ -10,5 +10,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/add", methods=["GET", "POST"])
+def add_task():
+    return render_template("add_task.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
