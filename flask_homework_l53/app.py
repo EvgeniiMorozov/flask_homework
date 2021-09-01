bc6d498a08144dea1480f273d0a0afc3ee444436
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-01 11:53:31 +0500

    add model Tasks

--- flask_homework_l53/app.py
+++ flask_homework_l53/app.py
@@ -3,6 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Tasks(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(45), nullable=False)
+    description = db.Column(db.Text)
+    is_completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return self.title
 
 
 @app.route("/")
