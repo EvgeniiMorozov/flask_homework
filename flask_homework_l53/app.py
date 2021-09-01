04d83827c3bc150d1144de7c36f87b2345c71432
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-01 14:23:42 +0500

    modify func add_task

--- flask_homework_l53/app.py
+++ flask_homework_l53/app.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
 
 class Tasks(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(45), nullable=False)
     description = db.Column(db.Text)
-    is_completed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return self.title
@@ -26,7 +25,19 @@ def index():
 
 @app.route("/add", methods=["GET", "POST"])
 def add_task():
-    return render_template("add_task.html")
+    if request.method != "POST":
+        return render_template("add_task.html")
+    title = request.form["task_title"]
+    description = request.form["task_description"]
+    if not title or not description:
+        return render_template("add_task.html", error="Ввведите данные правильно!")
+    task = Tasks(title=title, description=description)
+    try:
+        db.session.add(task)
+        db.session.commit()
+        return redirect("/add")
+    except:
+        return "Что-то пошло не так!"
 
 
 if __name__ == "__main__":
